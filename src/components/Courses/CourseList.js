e7b094b161c1c3c76c96e6b84b147c498974f897
cameron28202 <cameron28202@gmail.com>
2024-10-22 15:03:29 -0500

    group courses by subject

--- src/components/Courses/CourseList.js
+++ src/components/Courses/CourseList.js
@@ -1,22 +1,44 @@
-import React from "react";
-import CourseItem from "./CourseItem";
-import './CourseList.css'
+import React, { useMemo } from 'react';
+import CourseItem from './CourseItem';
+import './CourseList.css';
 
 const CourseList = ({ courses }) => {
-    if(courses.length === 0){
-        return <div className="error">No courses to display.</div>
+
+    const groupedCourses = useMemo(() => {
+        return courses.reduce((groups, course) => {
+            const subject = course.subject || 'Other';
+            if (!groups[subject]) {
+                groups[subject] = [];
+            }
+            groups[subject].push(course);
+            return groups;
+        }, {});
+    }, [courses]);
+
+    if (courses.length === 0) {
+        return <div className="error">No courses to display.</div>;
     }
 
     return (
-        <ul className="courses-list">
-            {courses.map(course => (
-                <CourseItem
-                    key={course.id}
-                    course={course}
-                />
-            ))}
-        </ul>
-    )
-}
+        <>
+            {Object.entries(groupedCourses)
+                .map(([subject, subjectCourses]) => (
+                    <div key={subject} className="subject-group">
+                        <h2 className="subject-title">{subject}</h2>
+                        <ul className="courses-list">
+                            {subjectCourses
+                                .sort((a, b) => a.id.localeCompare(b.id))
+                                .map(course => (
+                                    <CourseItem
+                                        key={course.id}
+                                        course={course}
+                                    />
+                                ))}
+                        </ul>
+                    </div>
+                ))}
+        </>
+    );
+};
 
 export default CourseList;
